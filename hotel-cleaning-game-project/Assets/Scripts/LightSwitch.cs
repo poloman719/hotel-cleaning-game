df8faeb603521085fb00732d906b5656cc289ade
DosenOff <shawnlee534@gmail.com>
2025-08-26 22:45:18 -0700

    LightSwitch + minor additions to hotelRoom

--- hotel-cleaning-game-project/Assets/Scripts/LightSwitch.cs
+++ hotel-cleaning-game-project/Assets/Scripts/LightSwitch.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour, IInteractable
 {
-    [SerializeField] private GameObject ceilingLight;
-    [SerializeField] private Animator ceilingLightAnim;
-    [SerializeField] private Transform parentTransform;
+    private GameObject ceilingLightPoint;
+    private GameObject ceilingLightDir;
+    private Animator ceilingLightAnim;
+    private Transform parentTransform;
 
     bool switchState = false;
 
     void Start()
     {
         parentTransform = transform.parent;
-        ceilingLight = parentTransform.Find("CeilingFan").GetChild(0).gameObject;
-        ceilingLightAnim = parentTransform.Find("CeilingFan").GetChild(1).GetComponent<Animator>();
+        ceilingLightPoint = parentTransform.Find("CeilingFan").GetChild(0).gameObject;
+        ceilingLightDir = parentTransform.Find("CeilingFan").GetChild(1).gameObject;
+        ceilingLightAnim = parentTransform.Find("CeilingFan").GetChild(2).GetComponent<Animator>();
     }
 
     public void Interact()
@@ -29,14 +31,18 @@ public class LightSwitch : MonoBehaviour, IInteractable
 
     void TurnOn()
     {
-        ceilingLight.SetActive(true);
+        ceilingLightPoint.SetActive(true);
+        ceilingLightDir.SetActive(true);
+
         ceilingLightAnim.SetFloat("FanSpeed", 1f);
         transform.rotation = Quaternion.Euler(180f, 0f, 0f);
     }
 
     void TurnOff()
     {
-        ceilingLight.SetActive(false);
+        ceilingLightPoint.SetActive(false);
+        ceilingLightDir.SetActive(false);
+
         ceilingLightAnim.SetFloat("FanSpeed", 0f);
         transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
