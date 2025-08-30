28db77209767c5809c40626a3a88175ef1c3989e
DosenOff <shawnlee534@gmail.com>
2025-08-30 01:16:08 -0700

    minor update to raycast script

--- hotel-cleaning-game-project/Assets/Scripts/RaycastManager.cs
+++ hotel-cleaning-game-project/Assets/Scripts/RaycastManager.cs
@@ -46,6 +46,9 @@ public class RaycastManager : MonoBehaviour
             else
                 reticleAnim.SetBool("detecting", false);
         }
+
+        else
+            reticleAnim.SetBool("detecting", false);
     }
 }
 
